edf07e3663460e080a8a340514e2db5d175d950a
zhubingbing <zhu.bingbing@99cloud.net>
2017-09-18 12:45:05 +0800

    add heapster

---
+++ heapster/deploy/1.c
@@ -0,0 +1,4 @@
+10.0.0.6 xi-n572pcblrm-0-igb3qm6kyezc-kube-minion-6liowirbpqtf
+10.0.0.5 xi-n572pcblrm-1-yk4anhyb7nqd-kube-minion-equ66bypztga
+10.0.0.11 xi-n572pcblrm-2-jzdwrgmtzovp-kube-minion-jajm566frh2u
+10.0.0.4 xi-sn4cadtj3s-0-fsk2yfc7ypna-kube-master-vl5wq2lzlatz
